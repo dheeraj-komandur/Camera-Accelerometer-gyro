b3f487ca154d2ecd6f6c84a863e5f92c5e579ed9
Dheeraj Komandur <dheerajkomandur@gmail.com>
2019-04-20 15:10:40 +0530

    Camera + Acc Graph on one screen

---
+++ app/src/main/java/com/dheeraj/accgyrocam/MainActivity.java
@@ -0,0 +1,417 @@
+package com.dheeraj.accgyrocam;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, SurfaceHolder.Callback, Camera.PictureCallback{
+
+    //Acc Start
+    private static final String TAG = "MainActivity";
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mgyroscope;
+    private  Sensor sensors;
+
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
+    //Acc End
+    //cam start
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+
+    public static final int REQUEST_CODE = 100;
+
+    private SurfaceView surfaceView;
+
+    private String[] neededPermissions = new String[]{CAMERA, WRITE_EXTERNAL_STORAGE};
+    //cam end
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //cam start
+        surfaceView = findViewById(R.id.surfaceView);
+        if (surfaceView != null) {
+            boolean result = checkPermission();
+            if (result) {
+                setupSurfaceHolder();
+            }
+        }
+        //cam end
+
+
+        //acc start
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mgyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for(int i=0; i<sensors.size(); i++){
+            Log.d(TAG, "onCreate: Sensor "+ i + ": " + sensors.get(i).toString());
+        }
+        if (mAccelerometer != null) {
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+            mSensorManager.registerListener(this, mgyroscope, SensorManager.SENSOR_DELAY_GAME);
+
+        }
+        mChart = (LineChart) findViewById(R.id.chart1);
+        mChart.getDescription().setEnabled(true);
+        mChart.setTouchEnabled(true);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+        mChart.setPinchZoom(true);
+        mChart.setBackgroundColor(Color.WHITE);
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+        mChart.setData(data);
+        Legend l = mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMaximum(10f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+        feedMultiple();
+        //acc end
+    }
+
+    //acc function start
+    private void addEntry(SensorEvent event) {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
+            data.addEntry(new Entry(set.getEntryCount(), event.values[0] + 5), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(150);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null){
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        if(plotData){
+            addEntry(event);
+            plotData = false;
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mSensorManager.unregisterListener(MainActivity.this);
+        thread.interrupt();
+        super.onDestroy();
+    }
+    //acc fun end
+
+    //cam fun start
+    private boolean checkPermission() {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            ArrayList<String> permissionsNotGranted = new ArrayList<>();
+            for (String permission : neededPermissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    permissionsNotGranted.add(permission);
+                }
+            }
+            if (permissionsNotGranted.size() > 0) {
+                boolean shouldShowAlert = false;
+                for (String permission : permissionsNotGranted) {
+                    shouldShowAlert = ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+                }
+                if (shouldShowAlert) {
+                    showPermissionAlert(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]));
+                } else {
+                    requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]));
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void showPermissionAlert(final String[] permissions) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle(R.string.permission_required);
+        alertBuilder.setMessage(R.string.permission_message);
+        alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                requestPermissions(permissions);
+            }
+        });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    private void requestPermissions(String[] permissions) {
+        ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                for (int result : grantResults) {
+                    if (result == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(MainActivity.this, R.string.permission_warning, Toast.LENGTH_LONG).show();
+                        setViewVisibility(R.id.showPermissionMsg, View.VISIBLE);
+                        return;
+                    }
+                }
+
+                setupSurfaceHolder();
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void setViewVisibility(int id, int visibility) {
+        View view = findViewById(id);
+        if (view != null) {
+            view.setVisibility(visibility);
+        }
+    }
+
+    private void setupSurfaceHolder() {
+        setViewVisibility(R.id.startBtn, View.VISIBLE);
+        setViewVisibility(R.id.surfaceView, View.VISIBLE);
+
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        setBtnClick();
+    }
+
+    private void setBtnClick() {
+        Button startBtn = findViewById(R.id.startBtn);
+        if (startBtn != null) {
+            startBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    captureImage();
+                }
+            });
+        }
+    }
+
+    public void captureImage() {
+        if (camera != null) {
+            camera.takePicture(null, null, this);
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        startCamera();
+    }
+
+    private void startCamera() {
+        camera = Camera.open();
+        camera.setDisplayOrientation(90);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        resetCamera();
+    }
+
+    public void resetCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            // Return if preview surface does not exist
+            return;
+        }
+
+        if (camera != null) {
+            // Stop if preview surface is already running.
+            camera.stopPreview();
+            try {
+                // Set preview display
+                camera.setPreviewDisplay(surfaceHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Start the camera preview...
+            camera.startPreview();
+        }
+    }
+
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    public void onPictureTaken(byte[] bytes, Camera camera) {
+        saveImage(bytes);
+        resetCamera();
+    }
+
+    private void saveImage(byte[] bytes) {
+        FileOutputStream outStream;
+        try {
+            String fileName = "TUTORIALWING_" + System.currentTimeMillis() + ".jpg";
+            File file = new File(Environment.getExternalStorageDirectory(), fileName);
+            outStream = new FileOutputStream(file);
+            outStream.write(bytes);
+            outStream.close();
+            Toast.makeText(MainActivity.this, "Picture Saved: " + fileName, Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //cam fun end
+
+}
